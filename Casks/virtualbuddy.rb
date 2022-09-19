46f685b65f7cc6d7f2b3e7a7960b134ce861151d
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2022-09-19 10:51:49 +0300

    wip: virtualbuddy

---
+++ Casks/virtualbuddy.rb
@@ -0,0 +1,11 @@
+cask "virtualbuddy" do
+  version "4"
+  sha256 "1da221800bbacdad25cedaae93cb57d0fc074c22d3e6e317974fbf36f4721c79"
+
+  url "https://github.com/insidegui/VirtualBuddy/releases/download/1.0.2/VirtualBuddy_v1.0.2-#{version}.dmg"
+  name "virtualbuddy"
+  desc ""
+  homepage ""
+
+  app "VirtualBuddy.app"
+end
